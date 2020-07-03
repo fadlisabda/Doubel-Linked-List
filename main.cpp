a1e49f57f65e2236b1e8960da604ed56fefe147c
fadlisabda <fadlisabdad@yahoo.com>
2020-07-03 15:45:53 +0700

    menambahkan fungsi untuk membuat node pada bagian tail/belakang dan menambahkan&merubah komentar

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
-class node{//membuat node
+//membuat node
+class node{
    public:
       int value;//memberi data/nilai pada node
       node *next;//pointer untuk menuju node berikutnya
       node *prev;//pointer untuk menuju node sebelumnya
    
-   node(int val,node *n,node *p)//mendefenisikan data dan pointer
+   //mendefenisikan data dan pointer next dan prev
+   node(int val,node *n,node *p)
    {
       value=val;
       next=n;
@@ -22,17 +24,19 @@ class node{//membuat node
    }
 };
 
-class double_list{//class untuk membuat prototype
+//class untuk membuat prototype
+class double_list{
    public:
       void inserthead(int value);//membuat node pada bagian head
-      void insertail(int val);//membuat node pada bagian tail
+      void inserttail(int val);//membuat node pada bagian tail
       void insertbefore(int value,int pos);//membuat node pada posisi yang diinginkan
       void removehead();//menghapus node pada bagian head
       void removetail();//menghapus node pada bagian tail
       void removepost(int pos);//menghapus node pada posisi yang diinginkan
       void print();//menampilkan seluruh node
 
-   double_list(){//mendefenisikan pointer head dan tail null
+   //mendefenisikan pointer head dan tail null
+   double_list(){
       head=NULL;
       tail=NULL;
    }
@@ -45,29 +49,49 @@ class double_list{//class untuk membuat prototype
          //mengembalikan 0 jika node berisi
 };
 
-int double_list::isempty(){//untuk mengembalikan nilai 1 atau 0
-   if(head==NULL||tail==NULL){//jika node kosong return 1
-      return 1;
+//untuk mengembalikan nilai 1 atau 0
+int double_list::isempty(){
+   if(head==NULL||tail==NULL){
+      return 1;//jika node kosong return 1
    }
-   else{//jika node berisi return 0
-      return 0;
+   else{
+      return 0;//jika node berisi return 0
    }
 }
 
-void double_list::inserthead(int val){//membuat node pada bagian head
+//membuat node pada bagian head/depan
+void double_list::inserthead(int val){
    if(isempty()==1){//jika node kosong
-      current=new node(val);//membuat node current berisi nilai val
+      current = new node(val);//membuat node current berisi nilai val
       head=current;//nilai dalam node current menjadi head
       tail=current;//nilai dalam node current menjadi tail
       tail->prev=NULL;//node setelah tail 0
       head->next=NULL;//node sebelum head 0
    }
    else{
-      current=new node();//membuat node dengan nilai/data kosong
+      current = new node();//membuat node dengan nilai/data kosong
       current->next=this->head;//meletakkan node didepan head
       this->head->prev=current;//meletakkan head di belakang node baru 
       current->value=val;//memberi nilai/data pada node baru
       this->head=current;//memberi node baru head
+   }
+}
+
+//membuat node pada bagian tail/belakang
+void double_list::inserttail(int val){
+   if(isempty()==1){//jika node kosong
+      current = new node(val);//membuat node current berisi nilai val
+      head=current;//menjadikan node head
+      tail=current;//menjadikan node tail
+      tail->next=NULL;//node tail berikutnya 0
+   }
+   else{
+      current = new node();//membuat node baru
+      current->prev=this->tail;//membuat node dibelakang tail
+      this->tail->next=current;//membuat node didepan tail
+      current->value=val;//membuat nilai/data pada node baru
+      this->tail=current;//memberi node baru tail
+      current->next=NULL;//node berikutnya 0
    }
 }
 
