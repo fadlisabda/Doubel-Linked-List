b4e125a482cca0be2f5059764643a2ba2ae1d073
fadlisabda <fadlisabdad@yahoo.com>
2020-07-03 14:28:45 +0700

    menambahkan class double_list dan komentar

--- main.cpp
+++ main.cpp
@@ -22,7 +22,28 @@ class node{//membuat node
    }
 };
 
+class double_list{//class untuk membuat prototype
+   public:
+      void inserthead(int value);//membuat node pada bagian head
+      void insertail(int val);//membuat node pada bagian tail
+      void insertbefore(int value,int pos);//membuat node pada posisi yang diinginkan
+      void removehead();//menghapus node pada bagian head
+      void removetail();//menghapus node pada bagian tail
+      void removepost(int pos);//menghapus node pada posisi yang diinginkan
+      void print();//menampilkan seluruh node
 
+   double_list(){//mendefenisikan pointer head dan tail null
+      head=NULL;
+      tail=NULL;
+   }
+
+   private:
+         node *head;//membuat pointer head
+         node *tail;//membuat pointer tail
+         node *current;//membuat pointer current
+         int isempty();//mengembalikan 1 jika node kosong
+         //mengembalikan 0 jika node berisi
+};
 
 int main(){
    
