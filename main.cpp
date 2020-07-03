14d1c729ca845fc2e9f97166f4bee237bcecc14d
fadlisabda <fadlisabdad@yahoo.com>
2020-07-03 13:59:51 +0700

    menambahkan class node

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@ class node{
       int value;
       node *next;
       node *prev;
-      
+   
+   node(int val,node *n,node *p)
+   {
+      value=val;
+      next=n;
+      prev=p;
+   }   
+
+   node(int v){
+      value=v;
+   }
+
+   node(){
+
+   }
 };
 
 int main(){
